cbcee5beefe59d9c07b6e848e0403ad0b7e9d755
Bishopsmove <jownby@ownby.net>
2013-05-10 00:20:19 -0400

    Added jquery-ui widget-specific files, updated knockout-mapping file, updated bundleconfig

--- Source/OpportunityTrackerSample/App_Start/BundleConfig.cs
+++ Source/OpportunityTrackerSample/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace OpportunityTrackerSample
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-{version}.js")
+                        .IncludeDirectory("~/Scripts/jquery-ui/", "jquery.ui.*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
@@ -31,21 +32,24 @@ namespace OpportunityTrackerSample
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                        .Include("~/Content/site.css")
+                        .Include("~/Content/toastr.css"));
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/jqueryui/themes/base").Include(
+                        "~/Content/themes/base/jquery.ui.all.css"));
+                        //"~/Content/themes/base/jquery.ui.core.css",
+                        //"~/Content/themes/base/jquery.ui.resizable.css",
+                        //"~/Content/themes/base/jquery.ui.selectable.css",
+                        //"~/Content/themes/base/jquery.ui.accordion.css",
+                        //"~/Content/themes/base/jquery.ui.autocomplete.css",
+                        //"~/Content/themes/base/jquery.ui.button.css",
+                        //"~/Content/themes/base/jquery.ui.dialog.css",
+                        //"~/Content/themes/base/jquery.ui.slider.css",
+                        //"~/Content/themes/base/jquery.ui.tabs.css",
+                        //"~/Content/themes/base/jquery.ui.datepicker.css",
+                        //"~/Content/themes/base/jquery.ui.progressbar.css",
+                        //"~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
